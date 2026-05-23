95ac82d4605da366f68a2781bd84a09f9de64bb7
J Sayompoo <jsayompoo@Js-MacBook-Pro.local>
2026-05-24 00:39:42 +0700

    fix cert page

---
+++ app/cert/[id]/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "black",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      CERT PAGE
+    </main>
+  );
+}
