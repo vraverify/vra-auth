9d9667d6fb0fe0b5c47c649bb6b7c849d26c1ee3
J Sayompoo <jsayompoo@Js-MacBook-Pro.local>
2026-05-24 00:46:49 +0700

    connect supabase

--- app/cert/[id]/page.tsx
+++ app/cert/[id]/page.tsx
@@ -1,10 +1,40 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
 type Props = {
   params: {
     id: string
   }
 }
 
-export default function CertificatePage({ params }: Props) {
+export default async function CertificatePage({ params }: Props) {
+  const { data } = await supabase
+    .from("certificates")
+    .select("*")
+    .eq("certificate_id", params.id)
+    .single()
+
+  if (!data) {
+    return (
+      <main
+        style={{
+          minHeight: "100vh",
+          background: "black",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Certificate Not Found
+      </main>
+    )
+  }
+
   return (
     <main
       style={{
@@ -16,9 +46,11 @@ export default function CertificatePage({ params }: Props) {
     >
       <h1>VRA VERIFY</h1>
 
-      <p>Certificate ID:</p>
+      <h2>{data.certificate_id}</h2>
 
-      <h2>{params.id}</h2>
+      <p>{data.brand}</p>
+
+      <p>{data.model}</p>
     </main>
   )
 }
