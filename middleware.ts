c8eb6ccc4e8d0d2e6693372d8d604acd0c85042a
J Sayompoo <jsayompoo@Js-MacBook-Pro.local>
2026-05-28 01:11:24 +0700

    secure admin route

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+
+  const basicAuth = request.headers.get("authorization");
+
+  if (basicAuth) {
+
+    const authValue = basicAuth.split(" ")[1];
+
+    const [user, password] = atob(authValue).split(":");
+
+    if (
+      user === process.env.ADMIN_USER &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse(
+    "Authentication required",
+    {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    }
+  );
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
